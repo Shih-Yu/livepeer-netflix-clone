616452cf9746b1801c70dff61046f25504716526
Shih-Yu Hwang <shihyuhwang@gmail.com>
2023-01-14 15:19:32 -0600

    added rainbowconnect button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,32 +1,43 @@
 import Link from 'next/link';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export default function Navbar() {
   return (
     <>
-      <nav className='flex mt-5 justify-center mb-5'>
-        <Link
-          href='/movies/newReleases'
-          className='hover:bg-zinc-700 rounded px-3 py-1 mr-3 text-2xl'
-        >
-          New Releases
-        </Link>
-        <Link
-          href='/movies/nowPlaying'
-          className='hover:bg-zinc-700 rounded px-3 py-1 mr-3 text-2xl'
-        >
-          Now Playing
-        </Link>
-        <Link href='/movies/popular' className='hover:bg-zinc-700 rounded px-3 py-1 mr-3 text-2xl'>
-          Popular
-        </Link>
-        <Link href='/movies/topRated' className='hover:bg-zinc-700 rounded px-3 py-1 mr-3 text-2xl'>
-          Top Rated
-        </Link>
-        <Link href='/myList' className='hover:bg-zinc-700 rounded px-3 py-1 mr-3 text-2xl'>
-          My List
-        </Link>
-
-      </nav>
+      <div className='flex justify-end my-[30px] mr-[100px]'>
+        <ConnectButton />
+      </div>
+      <div className='flex mt-5 justify-center mb-5'>
+        <nav>
+          <Link
+            href='/movies/newReleases'
+            className='hover:bg-zinc-700 rounded px-3 py-1 mr-3 text-2xl'
+          >
+            New Releases
+          </Link>
+          <Link
+            href='/movies/nowPlaying'
+            className='hover:bg-zinc-700 rounded px-3 py-1 mr-3 text-2xl'
+          >
+            Now Playing
+          </Link>
+          <Link
+            href='/movies/popular'
+            className='hover:bg-zinc-700 rounded px-3 py-1 mr-3 text-2xl'
+          >
+            Popular
+          </Link>
+          <Link
+            href='/movies/topRated'
+            className='hover:bg-zinc-700 rounded px-3 py-1 mr-3 text-2xl'
+          >
+            Top Rated
+          </Link>
+          <Link href='/myList' className='hover:bg-zinc-700 rounded px-3 py-1 mr-3 text-2xl'>
+            My List
+          </Link>
+        </nav>
+      </div>
     </>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,58 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '../components/Layout'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Layout from '../components/Layout';
+// import { LivepeerConfig, createReactClient, studioProvider, ThemeConfig } from '@livepeer/react';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { polygon, polygonMumbai } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [polygonMumbai],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_API }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Livepeer-Netflix-Clone',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
+
+// const client = createReactClient({
+//   provider: studioProvider({ apiKey: process.env.LIVEPEER_API }),
+// });
+
+// const livepeerTheme: ThemeConfig = {
+//   colors: {
+//     accent: 'rgb(0, 145, 255)',
+//     containerBorderColor: 'rgba(0, 145, 255, 0.9)',
+//   },
+//   fonts: {
+//     display: 'Inter',
+//   },
+// };
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <WagmiConfig client={wagmiClient}>
+        {/* <LivepeerConfig client={client} theme={livepeerTheme}> */}
+              <RainbowKitProvider chains={chains} theme={darkTheme()}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+              </RainbowKitProvider>
+        {/* </LivepeerConfig> */}
+      </WagmiConfig>
     </>
   );
-  
-  
 }
